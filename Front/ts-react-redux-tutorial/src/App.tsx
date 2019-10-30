975e2417702759d8d2ef4203da4825a13076c14a
circlezero <jwy0565@gmail.com>
2019-10-31 00:02:18 +0900

    [Front] typescript redux tutorial

---
+++ Front/ts-react-redux-tutorial/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CounterContainer from "./containers/CounterContainer";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      {/* <TodoInsert />
+      <TodoList /> */}
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ Front/ts-react-redux-tutorial/src/containers/CounterContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Counter from "../components/Counter";
+
+function CounterContainer() {
+  return <Counter />;
+}
+
+export default CounterContainer;
